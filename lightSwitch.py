7219e83c8711ef86854fdefd65183fd14429691f
LindaNko <138465520+LindaNko@users.noreply.github.com>
2023-07-11 21:16:55 +0200

    Update and rename main.py to lightSwitch.py

--- lightSwitch.py
+++ lightSwitch.py
@@ -3,13 +3,15 @@ def Light():
     #ask for user input
     choice1 = str(input("is the light on or off?: "))
     #check if switch is on 
-    if choice1 == "on":
+    if choice1.lower() == "on":
       print("the light is on ") #on output to screen
       break
     #check else if switch is  off  
-    elif choice1 == "off":
+    elif choice1.lower() == "off":
       print("the light is off") #off output to screen
       break
 Light()    
+    
+   
     
   
